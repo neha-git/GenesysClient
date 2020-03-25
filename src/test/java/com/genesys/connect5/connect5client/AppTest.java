64fab04687b8177d646ae0d7c8646650b2d020ee
Neha Choudhary <neha@Neha.grindrod.in>
2020-03-25 21:26:36 +0000

    Committing client files

---
+++ src/test/java/com/genesys/connect5/connect5client/AppTest.java
@@ -0,0 +1,20 @@
+package com.genesys.connect5.connect5client;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
